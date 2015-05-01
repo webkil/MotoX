de1502a9e04d769c80852e109f6a0bcba9a1e6ad
seppeydavid <seppeydavid@gmail.com>
2015-05-01 17:36:58 +0200

    Ajout des vues

--- MotoX/Ajout_moto.swift
+++ MotoX/Ajout_moto.swift
@@ -1,26 +1,27 @@
 //
-//  ViewController.swift
+//  Ajout_moto.swift
 //  MotoX
 //
-//  Created by Macmini_Dagon on 30.04.15.
+//  Created by Macmini_Dagon on 01.05.15.
 //  Copyright (c) 2015 Macmini_Dagon. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class Ajout_moto: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    
 }
-
