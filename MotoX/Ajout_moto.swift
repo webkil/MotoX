827348f707aca7c860cb0c32848bbab1eefc55cc
seppeydavid <seppeydavid@gmail.com>
2015-05-02 21:47:17 +0200

    Base de données
    Ajout base de données
    Insertion en base de données
    info sur objectid en println

--- MotoX/Ajout_moto.swift
+++ MotoX/Ajout_moto.swift
@@ -11,9 +11,23 @@ import CoreData
 
 class Ajout_moto: UIViewController {
 
+    let managedObjectContext =
+    (UIApplication.sharedApplication().delegate
+        as! AppDelegate).managedObjectContext
+    
+    @IBOutlet var marquetextfield: UITextField!
+    @IBOutlet var modeltextfield: UITextField!
+    @IBOutlet var kilometragetextfield: UITextField!
+    @IBOutlet var cylindreetextfield: UITextField!
+    @IBOutlet var SaveButton: UIButton!
+    @IBOutlet var Precedent_button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //griser le bouton à l'ouverture
+    //    SaveButton.titleLabel?.opaque = true
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +36,34 @@ class Ajout_moto: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func Save_action(sender: AnyObject) {
+      //appel de la fonction de sauvegarde
+    save(self)
+        
+    }
+    
+    func save (AnyObject){
+        
 
+        let entityDescription =
+        NSEntityDescription.entityForName("Moto",
+            inManagedObjectContext: managedObjectContext!)
+
+   
+        let moto = Moto(entity: entityDescription!,
+            insertIntoManagedObjectContext: managedObjectContext)
+        
+        moto.marque = marquetextfield.text
+        moto.model = modeltextfield.text
+        moto.cylindree = cylindreetextfield.text
+        moto.kilometrage = kilometragetextfield.text
+        print(kilometragetextfield.text)
+        print(moto.objectID)
+       // func hello ViewController.indexOfAccessibilityElement(moto.objectID)
+      
+    }
+    
+    
     
 }
